6b4f695e0db28aa32a00977686186916682d1e72
Akshay Khole <akshay@reversehack.in>
2013-07-05 19:52:43 +0530

    Adding dummy script to output text to a file

--- web.js
+++ web.js
@@ -4,6 +4,12 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World! modified');
+  // #!/usr/bin/env node
+  // var fs = require('fs');
+  // var outfile = "hello.txt";
+  // var out = "Modify this script to write out something different.\n";
+  // fs.writeFileSync(outfile, out);
+  // console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
 });
 
 var port = process.env.PORT || 5000;
